edc99ed3198504d59bb0fc8d32cd30def7efc411
Mohammad Sabbaghian <msabbaghian@hotmail.com>
2024-11-28 21:27:52 +0000

    Deleted MauiApp1

--- MauiApp1/Models/ProjectsTags.cs
+++
@@ -1,9 +0,0 @@
-namespace MauiApp1.Models
-{
-    public class ProjectsTags
-    {
-        public int ID { get; set; }
-        public int ProjectID { get; set; }
-        public int TagID { get; set; }
-    }
-}
